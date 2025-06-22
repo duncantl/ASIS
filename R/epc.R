81c758663e361f3404962fd1e18b397be580832c
Duncan Temple Lang <duncan@r-project.org>
2025-06-22 12:28:52 -0700

    plots of histories. Fixed a tweak re. getLines to have a larger threshold. Combine histories. Add meta information to each history to identify committee and topic and when approved.

--- R/epc.R
+++ R/epc.R
@@ -1,30 +1,36 @@
-library(ReadPDF)
-library(XML)
+#library(ReadPDF)
+#library(XML)
 
 if(FALSE) {
-  zz = getEPCHistories()
+  zz = getHistories()
 }
 
 if(FALSE) {
     xml = list.files(pattern = "^2.*\\.xml$")
-    hh = lapply(xml, getEPCHistories)
+    hh = lapply(xml, getHistories)
     names(hh) =  gsub("[ _].*", "", xml)
-    hh
+    hh2 = hh[sapply(hh, length) > 0]
+
+    hh3 = combineHistories(unlist(hh, recursive = FALSE))
+    w = !is.na(hh3$date) & hh3$date > Sys.Date()
+    hh3$date[w] = as.Date("2023/11/21")
+    plot.Histories(hh3)
 }
 
 
 
-getEPCHistories =
+getHistories =
 function(doc = "2025.06.20_GC Meeting Call.xml")    
 {
     if(is.character(doc))
         doc = readPDFXML(doc)
 
     #p = xmlParent(xmlParent(h[[1]]))
-    xpathApply(doc, "//page[.//text()[normalize-space(.) = 'History:']]", getEPCHistory)
+ 
+    xpathApply(doc, "//page[.//text()[normalize-space(.) = 'History:']]", getHistory)
 }
 
-getEPCHistory =
+getHistory =
 function(page)    
 {
     p = page
@@ -33,11 +39,12 @@ function(page)
     #bx = getBBox(p, TRUE)
     #mar = margins(p)    
     bb = getBBox2(p, TRUE)
-    ll = getLines(p)
+    ll = getLines(p, marThreshold = 12)
     
     w = cut(bb$top, unique(c(ll$y0, Inf)))
 
     bb.inBox = subset(bb, top >= min(ll$y0) & (top + height) <= max(ll$y0))
+
 #    plot(density(bb.inBox$left))
 
     # d = density(bb.inBox$left)
@@ -47,14 +54,19 @@ function(page)
 
     g = split(bb, w)
     g = g[ sapply(g, nrow) > 0]
-    browser()
+#    browser()
     tmp = lapply(g, function(x) split( x$text,  cut(x$left, unique(c(0, mid - 10, Inf)))))
 
     # drop the remainder
     tmp = tmp[ - length(tmp) ]
 
-
-    mkHistoryDF(tmp)
+    ans = mkHistoryDF(tmp)
+    meta = mkHistoryMetaInfo(page, ans, bb)
+    attr(ans, "metaInfo") = meta
+    
+    ans = rbind(ans,
+                data.frame(date = as.Date(fixDate(meta$approved), "%B %d, %Y"), steps = "approved", text = NA))
+    ans
 }
 
 mkHistoryDF =
@@ -74,10 +86,16 @@ function(x)
     trimws(gsub("[[:space:]]+", " ", x) )
 
 MonthNames = months(as.Date(sprintf("2025/%d/1", 1:12)))
+
 fixDate =
 function(x)
 {
-    return(gsub("Novermber", "November", x))
+    x = gsub("Novermber", "November", x)
+    if(any(w <- grepl("^[0-9]+/[0-9]+/[0-9]+", x))) 
+       x[w] = format(as.Date(x[w], "%m/%d/%Y"), "%B %d, %Y")
+
+    return(x)
+    
     
     els =  strsplit(x, " ")
     m = sapply(els, `[`, 1)
@@ -100,4 +118,92 @@ function(x)
 }
 
 
+
+mkHistoryMetaInfo =
+function(page, h, bbox = getBBox2(page, TRUE))
+{
+    #    subj = getNodeSet(page, ".//text[. = 'SUBJECT:']")[[1]]
+    #    font = xmlGetAttr(subj, "font")
+    #    approved = getNodeSet(page, sprintf(".//text[contains(., 'APPROVED:') and @font = '%s']", font))[[1]]
+
+    ia = i = grep("APPROV(ED|AL):", bbox$text)
+    approved = toRight(bbox[i, ], bbox)
+
+    i = grep("SUBJECT:", bbox$text)
+    subj = toRight(bbox[i, ], bbox)
+
+
+#    committee = toupper(strsplit(h$text[1], " ")[[1]][1])
+    committee2 = gsub(" APPROV(ED|AL).*", "", bbox$text[ia])
+#    if(committee != committee2)     browser()
+    list(subject = subj,
+         approved = approved,
+         committee = committee2)
+    
+}
+
+toRight =
+function(row, bbox, asText = TRUE, threshold = 4)
+{
+   ans =  bbox[ bbox$left > (row$left + row$width) &
+                abs(bbox$top - row$top) < threshold &
+                 abs( (bbox$top + bbox$height) - (row$top + row$height)) < threshold, ]
+
+   if(asText)
+      trimws(paste(ans$text, collapse = " "))
+   else
+       ans
+}
+
+
+###########################################################################
+
+combineHistories =
+function(x)
+{
+    tmp = mapply(expandHistory, x, names(x), SIMPLIFY = FALSE)
+    ans = do.call(rbind, tmp)
+    ans$item = rep(1:length(x), sapply(tmp, nrow))
+    ans
+}
+
+expandHistory =
+function(x, label = "",  meta = attr(x, "metaInfo"))
+{
+    x$committee = meta$committee
+    x$topic = meta$subject
+    x$from = label
+    x
+}
+
+
+
+plot.Histories =
+function(x, ...)
+{
+    x$action = mkActions(x$steps)
+
+    ggplot(x, aes(x = date,
+                  y = ordered(item, unique(item)),
+                  color = committee,
+                  shape = action)
+           ) +
+        geom_line() +
+        geom_point() +
+        ylab("item") 
+#        labs(shape = "action")
+}
+
+mkActions =
+function(x)
+{
+    ans = rep("?", length(x))
+    w = !is.na(x)
+    ans[w] = capitalize(x[w])
+    ans
+}
+
+capitalize =
+function(x)    
+  paste0(toupper(substring(x, 1, 1)), substring(x, 2))
 
